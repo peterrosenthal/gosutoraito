63c5f14bff767da415415a464879b09f9bfd9cd3
Jared Myers <jamy3873@colorado.edu>
2019-10-31 17:30:55 -0600

    Relection prototype
    Basic mirrors and line rendering script

---
+++ Assets/CrystalSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject go = collision.gameObject;
+        print(go.name);
+    }
+
+    public void Activate()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void Deactivate()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
