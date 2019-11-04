390ac5a2887a9f976695fe2c0853e12bf23f9626
peter <godwooj17@penguin>
2019-11-04 15:16:31 -0700

    reorganize some folders and some documentation

--- process/README.md
+++ process/README.md
@@ -1,4 +1,5 @@
 # Process
+
 ## Values and Core Mechanics
 10/30/2019  
 ### Values:
@@ -9,3 +10,7 @@
 * Continuation
 ### Core Mechanic
 Place mirror shards on pedestals to reflect rays of light in specific places to solve a puzzle.
+
+## Initial Brainstorming
+10/28/2019  
+Ghost Light is a game about a lone samurai exploring the ruins of an ancient temple. Evil spirits haunt the grounds and the samurai can only defeat them using reflected light, rendering his once trusty sword useless against these spectral foes. The original concept was more combat oriented, and involved fighting other samurai within a mirror maze, but for this game, we felt it would be more engaging to implement puzzles revolving around the use of mirrors and light rays to uncover secrets and pathways hidden throughout the temple (the core mechanic). We also want the game's overall aesthetic to reflect the architectural and mythological tropes of Shinto temples.
---
+++ reflection-prototype/Assets/LaserReflect.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LaserReflect : MonoBehaviour
+{
+    public int _maxReflectionCount = 5;
+    public float _maxStepDistance = 200;
+    public bool _drawPrediction;
+
+    private LineRenderer _lineRenderer;
+    public List<Vector3> _lineVertices;
+    private Ray _ray;
+    private RaycastHit _hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineVertices = new List<Vector3>(_maxReflectionCount + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _lineVertices.Clear();
+        _lineVertices.Add(this.transform.position);
+        _ray = new Ray(_lineVertices[0], this.transform.forward);
+        if (Physics.Raycast(_ray, out _hit, _maxStepDistance))
+        {
+            if (_hit.collider.gameObject.tag == "Mirror")
+            {
+                ReflectLineRenderer(_lineVertices[0], this.transform.forward, _maxReflectionCount);
+                
+            }
+            else
+            {
+                _lineVertices.Add(_hit.point);
+                if (_hit.collider.gameObject.tag == "Switch")
+                {
+
+                }
+            }
+        }
+        else
+        {
+            _lineVertices.Add(this.transform.position + (this.transform.forward * _maxStepDistance));
+        }
+        _lineRenderer.positionCount = _lineVertices.Count;
+        _lineRenderer.SetPositions(_lineVertices.ToArray());
+    }
+
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.red;
+        Handles.ArrowHandleCap(0, this.transform.position + this.transform.forward * 0.25f, this.transform.rotation, 0.5f, EventType.Repaint);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, 0.25f);
+
+        if (_drawPrediction)
+        {
+            DrawPredictedReflectionPattern(this.transform.position + this.transform.forward * 0.75f, this.transform.forward, _maxReflectionCount);
+
+        }
+    }
+
+    void ReflectLineRenderer(Vector3 position, Vector3 direction, int reflectionsLeft)
+    {
+        if (reflectionsLeft == 0) return;
+
+       
+        Ray ray = new Ray(position, direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, _maxStepDistance))
+        {
+            if (hit.collider.gameObject.tag == "Mirror")
+            {
+                direction = Vector3.Reflect(direction, hit.normal);
+            }
+            else if (hit.collider.gameObject.tag == "Switch")
+            {
+                GameObject switchGO;
+                CrystalSwitch crystal;
+                switchGO = hit.collider.gameObject;
+                crystal = switchGO.GetComponent<CrystalSwitch>();
+                crystal.Activate();
+            }
+            else
+            {
+                //crystal.Deactivate();
+            }
+            position = hit.point;
+            _lineVertices.Add(position);
+        }
+        else
+        {
+            position += direction * _maxStepDistance;
+            _lineVertices.Add(position);
+            return;
+        }
+        
+        ReflectLineRenderer(position, direction, reflectionsLeft - 1);
+    }
+
+    void DrawPredictedReflectionPattern(Vector3 position, Vector3 direction, int reflectionsRemaining)
+    {
+        //Return if no more reflections
+        if (reflectionsRemaining == 0) return;
+
+        Vector3 startPos = position;
+
+        //Raycast to detect collider, reflect if mirror
+        Ray ray = new Ray(position, direction);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, _maxStepDistance) && hit.collider.gameObject.tag == "Mirror")
+        {
+            direction = Vector3.Reflect(direction, hit.normal);
+            position = hit.point;
+        }
+        else
+        {
+            position += direction * _maxStepDistance;
+        }
+
+        //Draw Line
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(startPos, position);
+
+        DrawPredictedReflectionPattern(position, direction, reflectionsRemaining - 1);
+    }
+}
