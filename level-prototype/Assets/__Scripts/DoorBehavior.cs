9125174f22dc459ebc137cb87ea6c9c46d1803f9
Jared Myers <jamy3873@colorado.edu>
2019-11-26 10:23:29 -0800

    basic puzzle and doors and shiz

---
+++ level-prototype/Assets/__Scripts/DoorBehavior.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehavior : MonoBehaviour
+{
+    public GameObject[] linkedSwitches;
+    private GameObject doorCube;
+    private bool _isOpen = false;
+
+    void Start()
+    {
+        doorCube = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckSwitches())
+        {
+            Open();
+            
+
+        }
+        else
+        {
+            Close();
+            
+        }
+    }
+
+    private bool CheckSwitches()
+    {
+        if(linkedSwitches.Length > 0)
+        {
+            foreach (GameObject crystal in linkedSwitches)
+            {
+                CrystalSwitch c = crystal.GetComponent<CrystalSwitch>();
+                if (!c._active) return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private void Open()
+    {
+        _isOpen = true;
+        doorCube.GetComponent<Renderer>().enabled = false;
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
+    private void Close()
+    {
+        _isOpen = false;
+        doorCube.GetComponent<Renderer>().enabled = true;
+        GetComponent<BoxCollider>().isTrigger = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("teleport");
+        }
+    }
+}
