46825218a0590f3cfba78387e6fb66a9744b6b93
kmetcalfe1 <35747070+kmetcalfe1@users.noreply.github.com>
2019-12-11 15:55:54 -0700

    logo and fixed ui

--- Assets/_scripts/ui.cs
+++ Assets/_scripts/ui.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class ui : MonoBehaviour
 {   public GameObject defaultt;
     public GameObject onShrineEnter;
+    public GameObject onShrineEnterBackup;
     public GameObject onByMirror;
     public GameObject onByPickupable;
     public GameObject onEditMirror;
     public GameObject onPedestalGhost;
     public GameObject onPedestalNonGhost;
     GameObject treee;
-
+    bool entered = false;
     Animator anim;
 
     // Start is called before the first frame update
@@ -25,9 +26,15 @@ public class ui : MonoBehaviour
     // This script will likely need access to your other scripts to check conditionals, I just dont have them in the file I'm working in :(
     void Update()
     {
-        if (!treee.activeInHierarchy)
+        if (!treee.activeInHierarchy && !entered)
         {
             anim.SetBool("shrineEntered", true);
+            entered = true;
+        }
+        if (!defaultt.activeSelf && !onShrineEnter.activeSelf && !onByMirror.activeSelf && !onByPickupable.activeSelf && !onEditMirror.activeSelf && !onPedestalGhost.activeSelf && !onPedestalNonGhost.activeSelf)
+        {
+            onShrineEnterBackup.SetActive(true);
+
         }
 
     }
